56fb7f5f789a3ba053888018f6180edce31555e9
Lenkeri <Lno4kamalt@yandex.ru>
2021-02-22 21:48:37 +0500

    изменение ползунков размера

--- main.js
+++ main.js
@@ -6,5 +6,12 @@ canvasElement.setAttribute('width', window.innerWidth);
 canvasElement.setAttribute('height', window.innerHeight - toolbar.offsetHeight);
 ctx.fillText('Some text', 10, 50);
 
+const sizeSetting = document.querySelector('.size-setting');
+const sizeValue = document.querySelector('.size-value')
+sizeSetting.oninput = function (){
+  sizeValue.textContent = sizeSetting.value;
+};
+const color = document.querySelector('.color');
+
 
 
